6b44f360202425ac5d9123d453e25cc2cff19f7d
Kevin Garcia <kg.gar91@gmail.com>
2020-03-21 21:14:06 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,4 @@
-def display_rainbow( ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts "R: #{{display_rainbow[0]}}"
-  puts "O: #{{display_rainbow[1]}}"
-  puts "Y: #{{display_rainbow[2]}}"
-  puts "G: #{{display_rainbow[3]}}"
-  puts "B: #{{display_rainbow[4]}}"
-  puts "I: #{{display_rainbow[5]}}"
-  puts "V: #{{display_rainbow[6]}}""
+def display_rainbow
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
